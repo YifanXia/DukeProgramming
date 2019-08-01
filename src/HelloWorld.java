ceeef54eb281d6d8b62bcdec9b5937d7f4845b9b
Yifan Xia <xiayifanisme@gmail.com>
2019-08-01 21:10:48 +0200

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+import edu.duke.*;
+
+public class HelloWorld {
+
+    public static void runHello() {
+        FileResource res = new FileResource("resources/hello_unicode.txt");
+        for (String line : res.lines()) {
+            System.out.println(line);
+        }
+    }
+
+    public static void main(String[] args) {
+        runHello();
+    }
+
+}
---
+++ src/Point.java
@@ -0,0 +1,7 @@
+public class Point {
+
+    private int x;
+    private int y;
+
+
+}
