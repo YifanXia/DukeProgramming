a033fd8551f63df9e28a7160f897fd11a633d7ca
Yifan Xia <xiayifanisme@gmail.com>
2019-08-08 17:23:01 +0200

    added some classes in Course2

--- src/Course2/CaesarBreaker.java
+++ src/Course2/CaesarBreaker.java
@@ -1,4 +1,53 @@
 package Course2;
 
+import edu.duke.FileResource;
+
 public class CaesarBreaker {
+
+    public int[] countLetters(String message) {
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        int[] counts = new int[alphabet.length()];
+        for (int i = 0; i < message.length(); i ++) {
+            int index = alphabet.indexOf(Character.toLowerCase(message.charAt(i)));
+            if (index != -1) {
+                counts[index] ++;
+            }
+        }
+        return counts;
+    }
+
+    public int indexOfMax(int[] values) {
+        int indMax = 0;
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] > values[indMax]) {
+                indMax = i;
+            }
+        }
+        return indMax;
+    }
+
+    public String decrypt(String encrypted) {
+        CaesarCipher cc = new CaesarCipher();
+        int[] letterCounts = countLetters(encrypted);
+        int maxIndex = indexOfMax(letterCounts);
+        int dKey = maxIndex - 4;
+        if (maxIndex < 4) {
+            dKey = 26 + dKey;
+        }
+        return cc.encrypt(encrypted, 26 - dKey);
+    }
+
+    public void testDecrypt() {
+        CaesarCipher cc = new CaesarCipher();
+        String message = "Just a test string with lots of eeeeeeeeeeeeeeeees";
+        String encrypted = cc.encrypt(message, 13);
+        System.out.println(decrypt(encrypted));
+        encrypted = "Gwpv c vbuq pvokki yfve iqqu qc bgbgbgbgbgbgbgbgbu";
+        System.out.println(decrypt(encrypted));
+    }
+
+    public static void main(String[] args) {
+        CaesarBreaker cb = new CaesarBreaker();
+        cb.testDecrypt();
+    }
 }
--- src/Course2/WordLengths.java
+++ src/Course2/WordLengths.java
@@ -1,4 +1,53 @@
 package Course2;
 
+import edu.duke.FileResource;
+
 public class WordLengths {
+
+    void countWordLengths(FileResource resource, int[] counts) {
+        for (String word: resource.words()) {
+            int largeLength = counts.length - 1;
+            int length = word.length();
+            if (!Character.isLetter(word.charAt(0))) {
+                length --;
+            }
+            if (!Character.isLetter(word.charAt(word.length()-1))) {
+                length --;
+            }
+            if (length > largeLength) {
+                counts[largeLength] ++;
+            }
+            else {
+                counts[length] ++ ;
+            }
+        }
+    }
+
+    public void testCountWordLengths() {
+        int[] lengthCounts = new int[31];
+        FileResource resource = new FileResource("resources/data/smallHamlet.txt");
+        countWordLengths(resource, lengthCounts);
+        for (int k = 1; k < lengthCounts.length; k ++) {
+            if (lengthCounts[k] > 0) {
+                System.out.println(lengthCounts[k] + " words of length " + k);
+            }
+        }
+        System.out.println("The most frequent word length is: " + indexOfMax(lengthCounts));
+    }
+
+    public int indexOfMax(int[] values) {
+        int indMax = 0;
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] > values[indMax]) {
+                indMax = i;
+            }
+        }
+        return indMax;
+    }
+
+    public static void main(String[] args) {
+        WordLengths wordLengths = new WordLengths();
+        FileResource resource = new FileResource("resources/data/smallHamlet.txt");
+        wordLengths.testCountWordLengths();
+    }
 }
