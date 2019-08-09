0d264d09962f20817d413aed4c0d775e47457f74
Yifan Xia <xiayifanisme@gmail.com>
2019-08-09 15:33:15 +0200

    added ExportData

--- src/Course1/ExportDataParser.java
+++ src/Course1/ExportDataParser.java
@@ -5,11 +5,6 @@ import org.apache.commons.csv.*;
 
 public class ExportDataParser {
 
-    public CSVParser tester() {
-        FileResource fr = new FileResource();
-        return fr.getCSVParser();
-    }
-
     public String countryInfo(CSVParser parser, String country) {
         for (CSVRecord record: parser) {
             if (record.get("Country").toLowerCase().equals(country.toLowerCase())) {
@@ -19,4 +14,46 @@ public class ExportDataParser {
         }
         return "";
     }
+
+    public void listExportersTwoProducts(CSVParser parser, String exportItem1, String exportItem2) {
+        for (CSVRecord record: parser) {
+            if (record.get("Exports").toLowerCase().contains(exportItem1.toLowerCase())
+                    && record.get("Exports").toLowerCase().contains(exportItem2.toLowerCase())) {
+                System.out.println(record.get("Country"));
+            }
+        }
+    }
+
+    public int numberOfExporters(CSVParser parser, String exportItem) {
+        int counts = 0;
+        for (CSVRecord record: parser) {
+            if (record.get("Exports").toLowerCase().contains(exportItem.toLowerCase())) {
+                counts ++;
+            }
+        }
+        return counts;
+    }
+
+    public void bigExporters(CSVParser parser, String amount) {
+        int amountLength = amount.length();
+        for (CSVRecord record: parser) {
+            if (record.get("Value (dollars)").length() > amountLength) {
+                System.out.println(record.get("Country") + " " + record.get("Value (dollars)"));
+            }
+        }
+    }
+
+    public void tester() {
+        FileResource fr = new FileResource("resources/csv/exports/exportdata.csv");
+        System.out.println(countryInfo(fr.getCSVParser(), "Nauru"));
+        listExportersTwoProducts(fr.getCSVParser(), "fish", "nuts");
+        System.out.println(numberOfExporters(fr.getCSVParser(), "sugar"));
+        bigExporters(fr.getCSVParser(), "$999,999,999,999");
+    }
+
+    public static void main(String[] args) {
+        ExportDataParser export = new ExportDataParser();
+        export.tester();
+    }
+
 }
